a7495eac31a376ab7150ed81892e3941d23c0a66
subfish-zhou <91206366+subfish-zhou@users.noreply.github.com>
2024-07-06 21:21:19 +0800

    fix lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -6,11 +6,3 @@ package «test_trace» where
 
 lean_lib «TestTrace» where
   -- add library configuration options here
-
-@[default_target]
-lean_exe «test_trace» where
-  root := `Main
-  -- Enables the use of the Lean interpreter by the executable (e.g.,
-  -- `runFrontend`) at the expense of increased binary size on Linux.
-  -- Remove this line if you do not need such functionality.
-  supportInterpreter := true
